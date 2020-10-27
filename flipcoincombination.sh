2ae241009aef645837c50d29e4acbdc555ee94d6
prathyushas31 <prathyushas1111@gmail.com>
2020-10-28 00:16:06 +0530

    store the triplet combination as well as percentage

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,36 +1,53 @@
-
 read -p "Enter no of times to flip single coin:" n
-echo "Doublet Combination"
-declare -A dictionary2=( [HH]=0 [HT]=0 [TH]=0 [TT]=0 )
+echo "Triplet Combination"
+
+declare -A dictionary3=(  [HHH]=0 [HHT]=0 [HTH]=0 [THH]=0 [HTT]=0 [THT]=0 [TTH]=0 [TTT]=0 )
 for(( flip = 1; flip <= n; flip++ ))
 do
     echo -n "Flip-$flip is "
-    (( toss = RANDOM % 4 ))
+    (( toss = RANDOM % 8 ))
     case $toss in
         0)
-            echo "Heads Heads"
-            (( dictionary2[HH]++ )) 
+            echo "Heads Heads Heads"
+            (( dictionary3[HHH]++ )) 
             ;;
         1)
-            echo "Heads Tails"
-            (( dictionary2[HT]++ ))
+            echo "Heads Heads Tails"
+            (( dictionary3[HHT]++ ))
             ;;
         2)
-            echo "Tails Heads"
-            (( dictionary2[TH]++ ))
+            echo "Heads Tails Heads"
+            (( dictionary3[HTH]++ ))
             ;;
         3)
-            echo "Tails Tails"
-            (( dictionary2[TT]++ ))
+            echo "Tails Heads Heads"
+            (( dictionary3[THH]++ ))
+            ;;
+        4)
+            echo "Heads Tails Tails"
+            (( dictionary3[HTT]++ )) 
+            ;;
+        5)
+            echo "Tails Heads Tails"
+            (( dictionary3[THT]++ ))
+            ;;
+        6)
+            echo "Tails Tails Heads"
+            (( dictionary3[TTH]++ ))
+            ;;
+        7)
+            echo "Tails Tails Tails"
+            (( dictionary3[TTT]++ ))
             ;;
     esac
 done
 
-for combination in ${!dictionary2[@]}
+for combination in ${!dictionary3[@]}
 do
-    count=${dictionary2[$combination]}
+    count=${dictionary3[$combination]}
     percentage=$( echo "$count $n" | awk '{printf "%.2f", $1 * 100 / $2}')
     echo "percentage of $combination is $percentage"   
 done
+
 
 
